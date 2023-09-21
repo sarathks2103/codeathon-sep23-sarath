2c7f5f8bdfaaf44e6cf55a3aecfdcedc69a638cc
Sarath K S <sarath.sasi@airindia.com>
2023-09-21 15:13:30 +0530

    Finished 3 rd

---
+++ program-1/ValidateIPAddress.js
@@ -0,0 +1,24 @@
+function isValidIPAddress(ipAddress) {
+    const parts = ipAddress.split('.');
+  
+    if (parts.length !== 4) {
+      return false;
+    }
+  
+    for (const part of parts) {
+      const value = parseInt(part, 10);
+  
+      if (isNaN(value) || value < 0 || value > 255) {
+        return false;
+      }
+  
+      if (value.toString() !== part) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  console.log(isValidIPAddress('255.255.0.0'));
+    console.log(isValidIPAddress('555.555.555.555'));
+    console.log(isValidIPAddress('256.255.0.0'));
